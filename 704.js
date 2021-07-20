f623033fd52a436f9f2c2df84191c717899e61d6
rocky <rocky.zhang@ximalaya.com>
2021-07-20 11:10:55 +0800

    feat: binarySearch

---
+++ 704.js
@@ -0,0 +1,25 @@
+/**
+ * 二分查找
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var binarySearch = function (nums, target) {
+  let start = 0;
+  let end = nums.length - 1;
+  while (start <= end) {
+    // 优化
+    let mid = Math.floor(start + ((end - start) >> 1));
+    // let mid = Math.floor((start + end) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] > target) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+  return -1;
+};
+
+console.log(binarySearch([1, 2, 4, 5, 7], 7));
