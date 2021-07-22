8195d8912b55f86d6f94f0825a1af4df6303c402
rocky <rocky.zhang@ximalaya.com>
2021-07-22 20:19:28 +0800

    feat: 二叉树前序遍历

---
+++ binaryTree.js
@@ -0,0 +1,45 @@
+/**
+ *
+ * 二叉树的前序，中序，后序遍历
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+
+//  输入：root = ['A', 'B', 'C', 'D', 'E', null,'F']
+//  输出：[A,B,D,E,C,F]
+const tree = {
+  val: "A",
+  left: {
+    val: "B",
+    left: {
+      val: "D",
+    },
+    right: {
+      val: "E",
+    },
+  },
+  right: {
+    val: "C",
+    right: {
+      val: "F",
+    },
+  },
+};
+var preorderTraversal = function (root, res = []) {
+  if (!root) return res;
+  res.push(root.val);
+  preorderTraversal(root.left, res);
+  preorderTraversal(root.right, res);
+  return res;
+};
+
+const x = preorderTraversal(tree);
+console.log("x", x);
