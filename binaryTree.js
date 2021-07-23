7cae762d056c7b0d24fd6bd0e8d2746aa188970a
rocky <rocky.zhang@ximalaya.com>
2021-07-23 11:26:27 +0800

    feat: binary tree traverse

--- binaryTree.js
+++ binaryTree.js
@@ -1,6 +1,9 @@
 /**
  *
  * 二叉树的前序，中序，后序遍历
+ */
+
+/**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
  *     this.val = (val===undefined ? 0 : val)
@@ -8,10 +11,29 @@
  *     this.right = (right===undefined ? null : right)
  * }
  */
-/**
- * @param {TreeNode} root
- * @return {number[]}
- */
+var preorderTraversal = function (root, res = []) {
+  if (!root) return res;
+  res.push(root.val);
+  preorderTraversal(root.left, res);
+  preorderTraversal(root.right, res);
+  return res;
+};
+
+function inorderTraversal(root, res = []) {
+  if (!root) return res;
+  inorderTraversal(root.left, res);
+  res.push(root.val);
+  inorderTraversal(root.right, res);
+  return res;
+}
+
+function postorderTraversal(root, res = []) {
+  if (!root) return res;
+  postorderTraversal(root.left, res);
+  postorderTraversal(root.right, res);
+  res.push(root.val);
+  return res;
+}
 
 //  输入：root = ['A', 'B', 'C', 'D', 'E', null,'F']
 //  输出：[A,B,D,E,C,F]
@@ -33,13 +55,5 @@ const tree = {
     },
   },
 };
-var preorderTraversal = function (root, res = []) {
-  if (!root) return res;
-  res.push(root.val);
-  preorderTraversal(root.left, res);
-  preorderTraversal(root.right, res);
-  return res;
-};
-
 const x = preorderTraversal(tree);
 console.log("x", x);
