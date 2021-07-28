f390ce44f426f034cffb54a81395fd9a0c81f84c
rocky <rocky.zhang@ximalaya.com>
2021-07-28 16:49:36 +0800

    feat: dfs

---
+++ 111.js
@@ -0,0 +1,65 @@
+/**
+ * 二叉树的最小深度
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+// bfs
+var minDepth = function (root) {
+  if (!root) return 0;
+  const queue = [root];
+  let min = 1;
+  while (queue.length) {
+    const len = queue.length;
+    for (let i = 0; i < len; i++) {
+      const node = queue.shift();
+      if (!node.left && !node.right) {
+        return min;
+      }
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    min++;
+  }
+};
+// dfs
+var minDepth2 = function (root) {
+  if (!root) return 0;
+  if (!root.left && !root.right) return 1;
+  if (root.left && !root.right) {
+    return 1 + minDepth2(root.left);
+  }
+  if (!root.left && root.right) {
+    return 1 + minDepth2(root.right);
+  }
+  return Math.min(minDepth2(root.left), minDepth2(root.right)) + 1;
+};
+
+const tree = {
+  val: "A",
+  left: {
+    val: "B",
+    left: {
+      val: "D",
+    },
+    right: {
+      val: "E",
+    },
+  },
+  right: {
+    val: "C",
+    right: {
+      val: "F",
+    },
+  },
+};
+
+const x = minDepth2(tree);
+console.log(x);
