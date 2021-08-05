7afe2cb5494c2b43b07ca5c6616e8ebfbd6f5e76
rocky <rocky.zhang@ximalaya.com>
2021-08-05 20:34:33 +0800

    feat: 链表两两交换相邻节点

--- 24.js
+++ 24.js
@@ -10,7 +10,25 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var swapPairs = function (head) {};
+var swapPairs = function (head) {
+  // 创建一个头部节点
+  const dummy = {};
+  dummy.next = head;
+  // 当前节点
+  let temp = dummy;
+  // temp后面没有节点或者只有一个节点时不在交换
+  while (temp.next !== null && temp.next.next !== null) {
+    const node1 = temp.next;
+    const node2 = temp.next.next;
+
+    temp.next = node2;
+    node1.next = node2.next;
+    node2.next = node1;
+    // 交换后移位
+    temp = node1;
+  }
+  return dummy.next;
+};
 
 const linkedList = {
   val: 1,
@@ -28,3 +46,6 @@ const linkedList = {
     },
   },
 };
+
+const x = swapPairs(linkedList);
+console.log(x);
