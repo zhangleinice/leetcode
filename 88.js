f45bc3abfba4e5f3fdf827c1a3dc62fa6e5339d4
rocky <rocky.zhang@ximalaya.com>
2021-08-25 14:38:33 +0800

    feat: 逆向双指针

--- 88.js
+++ 88.js
@@ -1,52 +1,68 @@
 /**
+ * 最优：逆向双指针
  * @param {number[]} nums1
  * @param {number} m
  * @param {number[]} nums2
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
+// 拼接，排序
 var merge = function (nums1, m, nums2, n) {
   return nums1
-    .slice(0, m - 1)
+    .slice(0, m)
     .concat(nums2)
     .sort((a, b) => a - b);
 };
-// nums1和nums2都是有序的，不用再排序
-// 不能增加空间复杂度
+// 正向双指针，时间 O(m+n),空间 O(m)
 var merge2 = function (nums1, m, nums2, n) {
-  let res = [];
-  nums1 = nums1.slice(0, m);
-  while (nums2.length) {
-    if (nums1[0] <= nums2[0]) {
-      res.push(nums1.shift());
+  let i = 0,
+    j = 0,
+    k = 0;
+  let nums = [];
+  // 从前往后，小的值赋值到nums前面里，指针后移
+  while (i < m && j < n) {
+    if (nums1[i] > nums2[j]) {
+      nums[k++] = nums2[j++];
     } else {
-      res.push(nums2.shift());
+      nums[k++] = nums1[i++];
     }
   }
-  for (let i = 0; i < res.length; i++) {
-    nums1[i] = res[i];
+  // 当短的数组用完了，长数组直接放进nums
+  while (i < m) {
+    nums[k++] = nums1[i++];
   }
-  return nums1;
+  while (j < n) {
+    nums[k++] = nums2[j++];
+  }
+  return nums;
 };
-// 优化
+
+// 逆向双指针  时间 O(m+n), 空间 O(1)
 var merge3 = function (nums1, m, nums2, n) {
-  let p = 0,
-    q = 0;
-  while (p <= m + n) {
-    if (nums1[p] < nums2[q] && nums1[p + 1] > nums2[q]) {
-      nums1.splice(p, 0, nums2.shift());
-      p++;
-    } else if (nums1[p + 1] < nums2[q]) {
-      p++;
+  let i = m - 1,
+    j = n - 1,
+    k = m + n - 1;
+  // 从后往前，大的值赋值到nums后面，指针前移
+  while (i >= 0 && j >= 0) {
+    if (nums1[i] > nums2[j]) {
+      nums1[k--] = nums1[i--];
     } else {
-      nums1.splice(p - 1, 0, nums2.shift());
+      nums1[k--] = nums2[j--];
     }
+  }
+  // 短数组比较完了，就直接放进nums1前面
+  while (i >= 0) {
+    nums1[k--] = nums1[i--];
+  }
+  while (j >= 0) {
+    nums1[k--] = nums2[j--];
   }
   return nums1;
 };
+
 const n1 = [1, 2, 3, 0, 0, 0];
 const n2 = [2, 5, 6];
-console.log(merge2(n1, 3, n2, 3));
+console.log(merge3(n1, 3, n2, 3));
 
 // 输入：nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
 // 输出：[1,2,2,3,5,6]
