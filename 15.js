0775153aad6c2f137c51c6183e8280529c24878b
rocky <zhanglei@innotechx.com>
2021-12-20 22:18:25 +0800

    feat: tree sum

---
+++ 15.js
@@ -0,0 +1,11 @@
+/**
+ * 1. 暴力三重循环
+ * 2. set，map （时间O(N2), 空间 O(N)）
+ * 3. 双指针 （时间O(N2)）
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+
+//  给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有和为 0 且不重复的三元组。
+
+var threeSum = function (nums) {};
