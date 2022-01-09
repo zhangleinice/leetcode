434621f5086929a4d01275c49234af5025f81563
rocky <zhanglei@innotechx.com>
2022-01-09 22:03:42 +0800

    feat: 路径总和

--- index.js
+++ index.js
@@ -17,3 +17,52 @@ let a = [1, 2, 3, 4, 4, 4, 5, 6, 7];
 function getFirstTarget(nums, target) {}
 
 console.log(binarySearch([1, 2, 4, 5, 7], 7));
+
+// 输入: s = "abcabcbb" 'baca'
+// 输出: 3
+// 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+
+/**
+ * @param {string} s
+ * @return {number}
+ * 双指针
+ * set
+ * map
+ */
+var lengthOfLongestSubstring = function (s) {
+  let window = new Set();
+  let l = 0,
+    r = 0,
+    len = s.length,
+    maxLen = 0;
+  while (r < len) {
+    if (window.has(s[r])) {
+      window.delete(s[l++]);
+    } else {
+      window.add(s[r]);
+      maxLen = Math.max(maxLen, r++ - l + 1);
+    }
+  }
+  return maxLen;
+};
+
+var lengthOfLongestSubstring2 = function (s) {
+  let window = new Map();
+  let l = 0,
+    r = 0,
+    len = s.length,
+    maxLen = 0;
+  while (r < len) {
+    // 重复的值未删除，重复值必须大于 l
+    if (window.has(s[r])) {
+      l = window.get(s[r]) + 1;
+      window.delete(s[r]);
+    } else {
+      window.set(s[r], r);
+      maxLen = Math.max(maxLen, r++ - l + 1);
+    }
+  }
+  return maxLen;
+};
+
+console.log(lengthOfLongestSubstring2("abcabcbb"));
