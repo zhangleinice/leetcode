9f0457296a7a1f90eb97ffa3f1c77b9a4fa1dadd
rocky <zhanglei@innotechx.com>
2022-01-13 22:23:53 +0800

    feat: 回溯加去重

--- 47.js
+++ 47.js
@@ -12,6 +12,8 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
+
+// 先排序，第一个值为重复的跳过
 var permuteUnique = function (nums) {
   const res = [];
   const arr = [];
@@ -22,6 +24,7 @@ var permuteUnique = function (nums) {
       return;
     }
     for (let i = 0; i < nums.length; i++) {
+      // !vis[i - 1]第一个重复的
       if (vis[i] || (i > 0 && nums[i] === nums[i - 1] && !vis[i - 1])) continue;
       arr.push(nums[i]);
       vis[i] = true;
@@ -32,6 +35,32 @@ var permuteUnique = function (nums) {
     }
   }
   nums.sort((x, y) => x - y);
+  dfs(0);
+  return res;
+};
+
+// !这个方案是错的，还是得先排序
+// 不用排序，第一个值为重复的跳过
+var permuteUnique2 = function (nums) {
+  const res = [];
+  const arr = [];
+  const vis = new Array(nums.length).fill(false);
+  function dfs(idx) {
+    if (idx === nums.length) {
+      res.push(arr.slice());
+      return;
+    }
+    for (let i = 0; i < nums.length; i++) {
+      // 走过的跳过；第一个节点为重复的也直接跳过
+      if (vis[i] || (i > 0 && arr.slice().includes(nums[i]))) continue;
+      arr.push(nums[i]);
+      vis[i] = true;
+      dfs(idx + 1);
+      vis[i] = false;
+      // 回溯
+      arr.pop();
+    }
+  }
   dfs(0);
   return res;
 };
