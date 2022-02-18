e765a1f4fcff46ff98f34e9eb402fd7274a43b0a
rocky <rocky.zhang@ximalaya.com>
2022-02-18 17:59:07 +0800

    feat: 未完

--- array2tree.js
+++ array2tree.js
@@ -2,59 +2,6 @@
  * 一维数组转成树形结构菜单
  */
 
-const list = [
-  { id: 20, parentId: 0, name: "一级菜单1" },
-  { id: 21, parentId: 0, name: "一级菜单2" },
-  { id: 22, parentId: 0, name: "一级菜单3" },
-  { id: 23, parentId: 0, name: "一级菜单4" },
-  { id: 24, parentId: 20, name: "二级菜单" },
-  { id: 25, parentId: 20, name: "二级菜单" },
-  { id: 26, parentId: 24, name: "三级菜单" },
-  { id: 27, parentId: 24, name: "三级菜单" },
-  { id: 28, parentId: 21, name: "二级菜单" },
-  { id: 29, parentId: 21, name: "二级菜单" },
-  { id: 30, parentId: 29, name: "三级菜单" },
-  { id: 31, parentId: 30, name: "四级菜单" },
-  { id: 32, parentId: 31, name: "五级菜单" },
-];
-
-// ==>
-
-// [
-//     { "id": 22, "parentId": 0, "name": "一级菜单3" },
-//     { "id": 23, "parentId": 0, "name": "一级菜单4" },
-//     { "id": 20, "parentId": 0, "name": "一级菜单1",
-//         "children": [
-//             { "id": 25, "parentId": 20, "name": "二级菜单" },
-//             { "id": 24, "parentId": 20, "name": "二级菜单",
-//                 "children": [
-//                     { "id": 26, "parentId": 24, "name": "三级菜单" },
-//                     { "id": 27, "parentId": 24, "name": "三级菜单" }
-//                 ]
-//             }
-//         ]
-//     },
-//     {
-//         "id": 21, "parentId": 0, "name": "一级菜单2",
-//         "children": [
-//             { "id": 28, "parentId": 21, "name": "二级菜单" },
-//             { "id": 29, "parentId": 21, "name": "二级菜单",
-//                 "children": [
-//                     { "id": 30, "parentId": 29, "name": "三级菜单",
-//                         "children": [
-//                             { "id": 31, "parentId": 30, "name": "四级菜单",
-//                                 "children": [
-//                                     { "id": 32, "parentId": 31, "name": "五级菜单" }
-//                                 ]
-//                             }
-//                         ]
-//                     }
-//                 ]
-//             }
-//         ]
-//     }
-// ]
-
 // function bfs(list) {
 //   const queue = [{ id: 0, children: [] }];
 //   const head = [{ id: 0, children: [] }];
@@ -125,6 +72,59 @@ const bfs = (list) => {
   }
   return levels[0][0].children;
 };
+
+const list = [
+  { id: 20, parentId: 0, name: "一级菜单1" },
+  { id: 21, parentId: 0, name: "一级菜单2" },
+  { id: 22, parentId: 0, name: "一级菜单3" },
+  { id: 23, parentId: 0, name: "一级菜单4" },
+  { id: 24, parentId: 20, name: "二级菜单" },
+  { id: 25, parentId: 20, name: "二级菜单" },
+  { id: 26, parentId: 24, name: "三级菜单" },
+  { id: 27, parentId: 24, name: "三级菜单" },
+  { id: 28, parentId: 21, name: "二级菜单" },
+  { id: 29, parentId: 21, name: "二级菜单" },
+  { id: 30, parentId: 29, name: "三级菜单" },
+  { id: 31, parentId: 30, name: "四级菜单" },
+  { id: 32, parentId: 31, name: "五级菜单" },
+];
+
+// ==>
+
+// [
+//     { "id": 22, "parentId": 0, "name": "一级菜单3" },
+//     { "id": 23, "parentId": 0, "name": "一级菜单4" },
+//     { "id": 20, "parentId": 0, "name": "一级菜单1",
+//         "children": [
+//             { "id": 25, "parentId": 20, "name": "二级菜单" },
+//             { "id": 24, "parentId": 20, "name": "二级菜单",
+//                 "children": [
+//                     { "id": 26, "parentId": 24, "name": "三级菜单" },
+//                     { "id": 27, "parentId": 24, "name": "三级菜单" }
+//                 ]
+//             }
+//         ]
+//     },
+//     {
+//         "id": 21, "parentId": 0, "name": "一级菜单2",
+//         "children": [
+//             { "id": 28, "parentId": 21, "name": "二级菜单" },
+//             { "id": 29, "parentId": 21, "name": "二级菜单",
+//                 "children": [
+//                     { "id": 30, "parentId": 29, "name": "三级菜单",
+//                         "children": [
+//                             { "id": 31, "parentId": 30, "name": "四级菜单",
+//                                 "children": [
+//                                     { "id": 32, "parentId": 31, "name": "五级菜单" }
+//                                 ]
+//                             }
+//                         ]
+//                     }
+//                 ]
+//             }
+//         ]
+//     }
+// ]
 
 const x = bfs(list);
 console.log(x);
